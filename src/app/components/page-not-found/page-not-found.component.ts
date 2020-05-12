63ee64a93076c77ab8d7bbaa3499f0777ddb933c
David Kim <kim.david.j@gmail.com>
2020-05-11 22:22:29 -0700

    Added Visitor module

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -9,7 +9,6 @@ export class PageNotFoundComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
--- src/app/modules/visitor/visitor.module.ts
+++ src/app/modules/visitor/visitor.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VisitorComponent } from './components/visitor/visitor.component';
-
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [VisitorComponent],
+  declarations: [
+    VisitorComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatTableModule
   ],
   exports: [
-    VisitorComponent
+    VisitorComponent,
   ]
 })
 export class VisitorModule { }
