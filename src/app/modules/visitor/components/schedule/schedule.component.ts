4fedb67d78f711e9f5cbccb09708548687562129
Frank Sun <franksun036@gmail.com>
2020-06-16 22:00:05 -0700

    Schedule page rebuild done
    only API left, will try Tejas's idea

--- src/app/modules/visitor/components/schedule/schedule.component.ts
+++ src/app/modules/visitor/components/schedule/schedule.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from '@app/core/api/api.service';
 import { MatDialog } from '@angular/material/dialog';
-import { MatSort, Sort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
 import { TeamDialogOverviewComponent } from '../../components/team-dialog-overview/team-dialog-overview.component';
 import { SpinnerService } from '@app/shared/services/spinner.service';
 
@@ -13,57 +11,45 @@ import { SpinnerService } from '@app/shared/services/spinner.service';
 })
 
 export class ScheduleComponent implements OnInit {
-  dataSource = SCHEDULE_DATA;
-  displayedColumns = [
-    'drawnum',
-    'drawdate',
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-  ];
-  columnsToDisplay: string[] = this.displayedColumns.slice();
+  dataSource = SCHEDULE_DATA1;
 
-  selectedEvent = null;
-  currentEvent = null;
-  events: any = [
-    // "event0", "event1", "event2", "event3", "event4", "event5"
+  displayedColumns: String[] = [
+    'drawinfo',
+    'track_a',
+    'track_b',
+    'track_c',
+    'track_d'
   ];
+  //columnsToDisplay: string[] = this.displayedColumns.slice();
   animal: string;
   name: string;
+  selectedEvent = null;
+  currentEvent = null;
+  currentEventId = null;
+
 
   constructor(private api: ApiService, public dialog: MatDialog, private spinner: SpinnerService) { }
 
   ngOnInit(): void {
-    this.spinner.on();
-
+    console.log("ABCDEFG");
+    console.log(SCHEDULE_DATA1);
     this.api
-      .getEvents()
-      .subscribe((res) => {
-        console.log('[DEBUG] ngOnInit() in schedule component:');
-        console.log(res);
+      .currentEventId
+      .subscribe((eventId) => {
+        this.currentEventId = eventId;
 
-        this.events = res;
-        this.selectedEvent = this.events[0];
+        this.api
+          .getEvents()
+          .subscribe((res: any) => {
+            console.log('[DEBUG] ngOnInit() in schedule component:');
+            console.log(res);
 
-        this.spinner.off();
+            this.selectedEvent = res[res.length - 1];
+            this.currentEvent = res;
+
+            this.spinner.off();
+          });
       });
-    // const query1 = "SELECT * FROM public.curlingevent ORDER BY id DESC";
-
-    // this.apiService.adHocQuery(query1).subscribe((res: any) => {
-    //   console.log(res);
-
-    //   this.selectedEvent = res.rows[0];
-    //   // console.log(res.rows[0]);
-    //   this.currentEvent = res.rows;
-    //   // console.log("signal");
-    //   // console.log(this.currentEvent);
-
-    //   this.spinner.off();
-    // })
-
-    // const query2 = "SELECT * FROM public.curlingevent ORDER BY id DESC WHERE bracket_id = ";
   }
 
   openDialog(): void {
@@ -87,134 +73,36 @@ export class ScheduleComponent implements OnInit {
   }
 }
 
-export interface DialogData {
-  animal: string;
-  name: string;
+// export interface DialogData {
+//   animal: string;
+//   name: string;
+// }
+
+
+export interface gameData {
+  gameId: Number;
+  name: String;
+  team1: String;
+  team1Id: Number;
+  team2: String;
+  team2Id: Number;
+  finished: Boolean;
+  winnerTo: Number;
 }
-
-
 export interface drawData {
-  drawnum: Number;
-  drawdate: String;
-  limit: Number;
-  A: [any, any, any, any];
-  B: [any, any, any, any];
-  C: [any, any, any, any];
-  D: [any, any, any, any];
-  E: [any, any, any, any];
+  drawId: Number;
+  drawName: String;
+  startTime: String;
+  videoUrl: String;
+  games: gameData[];
 }
 
-const SCHEDULE_DATA: drawData[] = [
-  { drawnum: 1, drawdate: '2020-01-10', limit: 5, A: ["A", 1, 1, 'D'], B: ["A", 1, 1, 'D'], C: ["A", 1, 1, 'D'], D: ["A", 1, 1, 'D'], E: ["A", 1, 1, 'D'] },
-  { drawnum: 2, drawdate: '2020-01-10', limit: 5, A: ["A", 1, 1, 'D'], B: ["A", 1, 1, 'D'], C: ["A", 1, 1, 'D'], D: ["A", 1, 1, 'D'], E: ["A", 1, 1, 'D'] },
-  { drawnum: 3, drawdate: '2020-01-10', limit: 5, A: ["A", 1, 1, 'D'], B: ["A", 1, 1, 'D'], C: ["A", 1, 1, 'D'], D: ["A", 1, 1, 'D'], E: ["A", 1, 1, 'D'] },
-  { drawnum: 4, drawdate: '2020-01-10', limit: 5, A: ["A", 1, 1, 'D'], B: ["A", 1, 1, 'D'], C: ["A", 1, 1, 'D'], D: ["A", 1, 1, 'D'], E: ["A", 1, 1, 'D'] },
-  { drawnum: 5, drawdate: '2020-01-10', limit: 5, A: ["A", 1, 1, 'D'], B: ["A", 1, 1, 'D'], C: ["A", 1, 1, 'D'], D: ["A", 1, 1, 'D'], E: ["A", 1, 1, 'D'] },
-  { drawnum: 6, drawdate: '2020-01-10', limit: 5, A: ["A", 1, 1, 'D'], B: ["A", 1, 1, 'D'], C: ["A", 1, 1, 'D'], D: ["A", 1, 1, 'D'], E: ["A", 1, 1, 'D'] },
-  { drawnum: 7, drawdate: '2020-01-10', limit: 5, A: ["A", 1, 1, 'D'], B: ["A", 1, 1, 'D'], C: ["A", 1, 1, 'D'], D: ["A", 1, 1, 'D'], E: ["A", 1, 1, 'D'] },
-  { drawnum: 8, drawdate: '2020-01-10', limit: 5, A: ["A", 1, 1, 'D'], B: ["A", 1, 1, 'D'], C: ["A", 1, 1, 'D'], D: ["A", 1, 1, 'D'], E: ["A", 1, 1, 'D'] },
-  { drawnum: 9, drawdate: '2020-01-10', limit: 5, A: ["A", 1, 1, 'D'], B: ["A", 1, 1, 'D'], C: ["A", 1, 1, 'D'], D: ["A", 1, 1, 'D'], E: ["A", 1, 1, 'D'] },
-  { drawnum: 10, drawdate: '2020-01-10', limit: 5, A: ["A", 1, 1, 'D'], B: ["A", 1, 1, 'D'], C: ["A", 1, 1, 'D'], D: ["A", 1, 1, 'D'], E: ["A", 1, 1, 'D'] },
-];
-/*
-export interface finalData {
-  drawnum: Number;
-  drawdate: String;
-  detail: String[];
-}
-
-const FINAL_DATA: finalData[] = []
-for (let i = 0; i < SCHEDULE_DATA.length; i++) {
-  FINAL_DATA[i].drawnum = SCHEDULE_DATA[i].drawnum;
-  FINAL_DATA[i].drawdate = SCHEDULE_DATA[i].drawdate;
-  for (let n = 0; i < SCHEDULE_DATA[i].limit; i++) {
-    FINAL_DATA[i].detail[n] = "Team: " + SCHEDULE_DATA[i].A[0] + '\n' + "Team: " + SCHEDULE_DATA[i].A[3] + '\n' + "Winner to " + SCHEDULE_DATA[i].A[1] + '\n' + "Losser to " + SCHEDULE_DATA[i].A[3];
-  }
-
-
-}
-*/
-/*
-export interface GameInfo {
-  GameNumber: Number;
-  Team_1: String;
-  Team_2: String;
-  WinTo: Number;
-  LossTo: Number;
-}
-
-export interface DrawInfo {
-  DrawNumber: number;
-  DrawDate: string;
-  GamesData: GameInfo[];
-}
-
-export interface ScheduleInfo {
-  FieldSize: 5;
-  MatchName: String;
-  DrawsData: DrawInfo[];
-}
-
-const SCHEDULE_DATA: ScheduleInfo = {
-  FieldSize: 5,
-  MatchName: "Test_Championships",
-  DrawsData: [],
-};
-var GAME_DATA: GameInfo[] = [];
-
-for (let i = 1; i < 11; i++) {
+const SCHEDULE_DATA1: drawData[] = []
+var GAME_DATA: gameData[] = []
+for (let i = 1; i < 10; i++) {
   GAME_DATA = [];
   for (let n = 1; n < 5; n++) {
-    GAME_DATA.push({
-      GameNumber: i * 5 + n,
-      Team_1: "Team_1",
-      Team_2: "Team_2",
-      WinTo: n + 5,
-      LossTo: n + 4,
-    })
+    GAME_DATA.push({ gameId: 4 * (i - 1) + n, name: "testid", team1: "team1", team2: "team2", team1Id: 1, team2Id: 2, finished: true, winnerTo: 20 });
   }
-  SCHEDULE_DATA.DrawsData.push({
-    DrawNumber: i,
-    DrawDate: "Anytime",
-    GamesData: GAME_DATA,
-  })
+  SCHEDULE_DATA1.push({ drawId: i, drawName: "Testgame", startTime: "Independence Day", videoUrl: "CCC", games: GAME_DATA });
 }
-*/
-/*
-export class TableDynamicColumnsExample {
-  displayedColumns: string[] = ['drawnum', 'drawdate', 'A', 'B', 'C', 'D', 'E'];
-  columnsToDisplay: string[] = this.displayedColumns.slice();
-  data: PeriodicElement[] = ELEMENT_DATA;
-}
-*/
-
-
-///////----------------------------------------------------------------------------
-
-/*
-export interface draw {
-  drawnum: Number;
-  drawtime: Date;
-  gamenumber: Number;
-  game1: [number, string, string, number, number];
-  game2: [number, string, string, number, number];
-  game3: [number, string, string, number, number];
-  game4: [number, string, string, number, number];
-  game5: [number, string, string, number, number];
-}
-const SCHEDULE_DATA: draw[] = [];
-
-// test data
-for (let i = 1; i < 10; i++) {
-  SCHEDULE_DATA.push({
-    drawnum: i,
-    drawtime: null,
-    gamenumber: 5,
-    game1: [11, 'teamA', 'teamB', 13, 15],
-    game2: [(i - 1) * 5 + 2, 'team_${i}', 'team_${i}', (i - 1) * 1 + 2, (i - 1) * 1 + 3],
-    game3: [(i - 1) * 5 + 3, 'team_${i}', 'team_${i}', (i - 1) * 1 + 3, (i - 1) * 1 + 4],
-    game4: [(i - 1) * 5 + 4, 'team_${i}', 'team_${i}', (i - 1) * 1 + 4, (i - 1) * 1 + 5],
-    game5: [(i - 1) * 5 + 5, 'team_${i}', 'team_${i}', (i - 1) * 1 + 5, (i - 1) * 1 + 0]
-  })
-}
-*/
