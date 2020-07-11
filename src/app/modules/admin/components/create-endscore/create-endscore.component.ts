2bbb6b668e20e2af3eaa785fbf910a10b1f8564b
Jean Choi <choijean@pdx.edu>
2020-07-11 09:31:08 -0700

    working on add endscore page

--- src/app/modules/admin/components/create-endscore/create-endscore.component.ts
+++ src/app/modules/admin/components/create-endscore/create-endscore.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { MatStepperModule } from '@angular/material/stepper';
+import { ApiService } from '@app/core/api/api.service';
+import { SpinnerService } from '@app/shared/services/spinner.service';
+import { NotificationService } from '@app/shared/services/notification.service';
+import { NgxMatNativeDateModule } from '@angular-material-components/datetime-picker';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-create-endscore',
   templateUrl: './create-endscore.component.html',
-  styleUrls: ['./create-endscore.component.scss']
+  styleUrls: ['./create-endscore.component.scss'],
 })
 export class CreateEndscoreComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
 
-  constructor() { }
+  eventNames: any[] = [];
+  games: any[] = [];
+  selectedEventId;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private api: ApiService,
+    private spinner: SpinnerService,
+    private notifier: NotificationService
+  ) {}
 
   ngOnInit(): void {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+
+    this.spinner.on();
+    this.api.getEvents().subscribe((res: any) => {
+      if (res === null || res === undefined) {
+        this.notifier.showError('Could not fetch curling events', 'ERROR');
+        this.spinner.off();
+        return;
+      }
+      this.spinner.off();
+      this.eventNames = res;
+      console.log('eventNames:');
+      console.log(this.eventNames);
+    });
   }
 
+  getEventGames() {
+    // this.selectedEventId = this.firstFormGroup.value.firstCtrl;
+    // console.log(`selectedEventId= ${this.selectedEventId}`);
+    // this.spinner.on();
+    // this.api.getGames(this.selectedEventId).subscribe((res: any) => {
+    //   this.games = res;
+    //   this.spinner.off();
+    //   console.log(`games`);
+    //   console.log(this.games);
+    // });
+  }
+
+  getGame() {}
+
+  onClickSubmit() {}
 }
