d94c06654865699af9a771e5c7127495ba2c1310
David Kim <kim.david.j@gmail.com>
2020-07-13 18:43:03 -0700

    Refactored game list in Admin dashboard

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+
+  // let disFunc = () => 'console has been disabled in production mode';
+
+  // console.log = disFunc
+  // console.error = disFunc
+  // console.warn = disFunc
+
+  // Object.freeze(console);
 }
 
 platformBrowserDynamic()
