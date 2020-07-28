049f87cf2b3612ab4e1a215e22d3369378842515
David Kim <kim.david.j@gmail.com>
2020-07-28 13:31:33 -0700

    Fixed team standings in Visitor page

--- src/app/modules/visitor/components/desktop-view/desktop-view.component.ts
+++ src/app/modules/visitor/components/desktop-view/desktop-view.component.ts
@@ -7,7 +7,6 @@ import {
 } from '@angular/core';
 import { ApiService } from '@app/core/api/api.service';
 import { MatDialog } from '@angular/material/dialog';
-import { TeamDialogOverviewComponent } from '@app/modules/visitor/components/team-dialog-overview/team-dialog-overview.component';
 import { YoutubeDialogComponent } from '@app/modules/visitor/components/youtube-dialog/youtube-dialog.component';
 import { MatTableDataSource } from '@angular/material/table';
 import { SpinnerService } from '@app/shared/services/spinner.service';
@@ -98,282 +97,17 @@ export class DesktopViewComponent implements OnInit {
               this.allScores = vals[2];
 
               this.selectedDraw = this.allDraws[this.allDraws.length - 1];
-              this.notificationService.showInfo(this.selectedDraw.video_url, '');  // DEBUGGING
+              // this.notificationService.showInfo(this.selectedDraw.video_url, '');  // DEBUGGING
 
               this.loadGames().then(res => {
                 this.spinnerService.off();
               });
+
+              this.loadTeamStandings();
             })
           );
 
         });
-
-    //         // Add dataSource key-value pair for Scores Tab
-    //         for (let game of this.allGames) {
-    //           game.dataSource = [
-    //             {
-    //               name: game.team_name1,
-    //               round_1: '0',
-    //               round_2: '2',
-    //               round_3: '0',
-    //               round_4: '0',
-    //               round_5: '2',
-    //               round_6: '0',
-    //               round_7: '1',
-    //               round_8: '0',
-    //               rounds: ['0',
-    //                 '2',
-    //                 '0',
-    //                 '0',
-    //                 '2',
-    //                 '0',
-    //                 '1',
-    //                 '0'],
-    //               final_score: '',
-    //             },
-    //             {
-    //               name: game.team_name2,
-    //               round_1: '0',
-    //               round_2: '0',
-    //               round_3: '2',
-    //               round_4: '1',
-    //               round_5: '0',
-    //               round_6: '2',
-    //               round_7: '0',
-    //               round_8: '5',
-    //               rounds: ['0',
-    //                 '2',
-    //                 '0',
-    //                 '0',
-    //                 '2',
-    //                 '0',
-    //                 '1',
-    //                 '0'],
-    //               final_score: '',
-    //             },
-    //           ];
-    //         }
-
-    //         this.currentGames = this.allGames.filter(
-    //           (x) => x.draw_id === this.selectedDraw.id
-    //         );
-
-    //         this.currentGames.forEach((game) =>
-    //           this.allScores.forEach((score) => {
-    //             if (score.game_id === game.game_id)
-    //               this.currentScores.push(score);
-    //           })
-    //         );
-
-    //         console.log('this.allGames:');
-    //         console.log(this.allGames);
-
-    //         /************************************************************************/
-
-    //         let pools;
-    //         let brackets;
-
-    //         await this.apiService.getPool(this.currentEventId)
-    //           .toPromise()
-    //           .then(
-    //             (res: any) => {
-    //               // console.log('pools:');
-    //               // console.log(res);
-
-    //               // Convert array of objects to object
-    //               let arr = res;
-    //               let result = {};
-    //               for (let i = 0; i < arr.length; i++) {
-    //                 result[arr[i].id] = {
-    //                   event_id: arr[i].event_id,
-    //                   name: arr[i].name,
-    //                   color: arr[i].color
-    //                 }
-    //               }
-
-    //               pools = result;
-    //               console.log('pools:');
-    //               console.log(pools);
-    //             });
-
-    //         await this.apiService.getBracket(this.currentEventId)
-    //           .toPromise()
-    //           .then(
-    //             (res: any) => {
-    //               // console.log('pools:');
-    //               // console.log(res);
-
-    //               // Convert array of objects to object
-    //               let arr = res;
-    //               let result = {};
-    //               for (let i = 0; i < arr.length; i++) {
-    //                 result[arr[i].id] = {
-    //                   event_id: arr[i].event_id,
-    //                   name: arr[i].name,
-    //                   color: arr[i].color
-    //                 }
-    //               }
-
-    //               brackets = result;
-    //               console.log('brackets:');
-    //               console.log(brackets);
-    //             });
-
-    //         this.allGames.forEach((e) => {
-    //           e.pool_name = pools[e.pool_id]?.name || null;
-    //           e.type = pools[e.pool_id]?.name ? 'Pool' : null;
-    //           e.bracket_name = pools[e.bracket_id]?.name || null;
-    //           e.type = pools[e.pool_id]?.name ? 'Bracket' : null;
-
-    //           e.label_name = pools[e.pool_id]?.name || pools[e.bracket_id]?.name || null;
-    //         })
-
-    //         // Get unique names of pools & brackets
-    //         let unique_names = [...new Set(this.allGames.map((e) => e.label_name))];
-    //         unique_names = unique_names.filter(e => e !== null);
-    //         unique_names = ['All Teams'].concat(unique_names);
-
-    //         console.log('unique_names:');
-    //         console.log(unique_names);
-
-    //         // Create team mapping
-    //         let team_mapping = {};
-    //         for (let game of this.allGames) {
-    //           if (!(team_mapping.hasOwnProperty(game.curlingteam1_id))) {
-    //             team_mapping[game.curlingteam1_id] = game.team_name1
-    //           }
-    //           if (!(team_mapping.hasOwnProperty(game.curlingteam2_id))) {
-    //             team_mapping[game.curlingteam2_id] = game.team_name2
-    //           }
-    //         }
-
-    //         console.log(team_mapping);
-
-    //         let A = this.allGames.filter((e) => e.label_name === unique_names[1]);
-
-    //         // this.poolBracketList = [
-    //         //   [
-    //         //     'pool 1', [
-    //         //       {
-    //         //         name: team_name1,
-    //         //         wins: 0,
-    //         //         losses: 0,
-    //         //         ties: 0
-    //         //       },
-    //         //       {
-    //         //         name: team_name2,
-    //         //         wins: 0,
-    //         //         losses: 0,
-    //         //         ties: 0
-    //         //       }
-    //         //     ]
-    //         //   ],
-    //         //   ['pool 2'],
-    //         //   ['pool 3'],
-    //         //   ['bracket 1'],
-    //         //   ['bracket 2']
-    //         // ]
-
-    //         for (let event_type_name of unique_names) {
-
-    //           // console.log(event_type_name);
-
-    //           // Get all games in a given pool or bracket
-    //           let games = this.allGames.filter((e) => e.label_name === event_type_name);
-
-    //           // Get all unique teams in a given pool or bracket
-    //           let teams = [...new Set(games.map((e) => e.curlingteam1_id).concat(games.map((e) => e.curlingteam2_id)))];
-
-    //           console.log(teams);
-
-    //           // Add a counter object for each team
-    //           let to_add = [
-    //             event_type_name,  // Cattle-A
-    //             []                // List of team standings
-    //           ];
-
-    //           for (let team_id of teams) {
-    //             to_add[1].push({
-    //               team_name: team_mapping[team_id],
-    //               team_id: team_id,
-    //               wins: 0,
-    //               losses: 0,
-    //               ties: 0
-    //             })
-    //           }
-
-    //           // Tally up the scores
-    //           for (let game of games) {
-
-    //             // Increment ties...
-    //             if (game.finished === true && game.winner === null) {
-    //               to_add[1].find(e => e.team_id === game.curlingteam1_id).ties++;
-    //               to_add[1].find(e => e.team_id === game.curlingteam2_id).ties++;
-    //             }
-
-    //             // Increment win...
-    //             else if (game.finished === true && game.winner === game.curlingteam1_id) {
-    //               to_add[1].find(e => e.team_id === game.curlingteam1_id).wins++;
-    //               to_add[1].find(e => e.team_id === game.curlingteam2_id).losses++;
-    //             }
-
-    //             // Increment losses...
-    //             else if (game.finished === true && game.winner === game.curlingteam2_id) {
-    //               to_add[1].find(e => e.team_id === game.curlingteam2_id).wins++;
-    //               to_add[1].find(e => e.team_id === game.curlingteam1_id).losses++;
-    //             }
-
-    //           }
-
-    //           this.poolBracketList.push(to_add);
-    //         }
-
-    //         // Combine all lists for All Teams list
-
-
-    //         console.log(this.poolBracketList);
-
-    //         let totals = {};
-    //         for (let arr of this.poolBracketList.slice(1)) {
-    //           for (let game of arr[1]) {
-    //             if (!(totals.hasOwnProperty(game.team_id))) {
-    //               // console.log('creating');
-    //               // console.log(game);
-
-    //               totals[game.team_id] = game
-    //             } else {
-    //               // console.log('adding');
-    //               // console.log(totals[game.team_id]);
-    //               // console.log(game);
-
-    //               // console.log('before');
-    //               // console.log(totals[game.team_id].wins);
-
-
-    //               totals[game.team_id].wins += game.wins;
-    //               totals[game.team_id].losses += game.losses;
-    //               totals[game.team_id].ties += game.ties;
-
-    //               // console.log('after');
-    //               // console.log(totals[game.team_id].wins);
-
-
-    //             }
-    //           }
-    //         }
-
-    //         console.log(totals);
-
-    //         this.selectedPoolBracket = this.poolBracketList[0][0];
-    //         this.poolBracketList[0][1] = Object.values(totals);
-    //         this.dataSourceStandings = Object.values(totals);
-
-    //         console.log('this.dataSourceStandings');
-
-    //         console.log('poolBracketList:');
-    //         console.log(this.poolBracketList);
-
-    //         this.spinnerService.off();
   }
 
   loadGames() {
@@ -424,14 +158,207 @@ export class DesktopViewComponent implements OnInit {
     });
   }
 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(TeamDialogOverviewComponent, {
-      width: 'auto',
-    });
+  async loadTeamStandings() {
 
-    dialogRef.afterClosed().subscribe((result) => {
-      // console.log('The dialog was closed');
-    });
+    let pools;
+    let brackets;
+
+    await this.apiService.getPool(this.currentEventId)
+      .toPromise()
+      .then(
+        (res: any) => {
+          // Convert array of objects to object
+          let arr = res;
+          let result = {};
+          for (let i = 0; i < arr.length; i++) {
+            result[arr[i].id] = {
+              event_id: arr[i].event_id,
+              name: arr[i].name,
+              color: arr[i].color
+            }
+          }
+
+          pools = result;
+          console.log('pools:');
+          console.log(pools);
+        });
+
+    await this.apiService.getBracket(this.currentEventId)
+      .toPromise()
+      .then(
+        (res: any) => {
+          // Convert array of objects to object
+          let arr = res;
+          let result = {};
+          for (let i = 0; i < arr.length; i++) {
+            result[arr[i].id] = {
+              event_id: arr[i].event_id,
+              name: arr[i].name,
+              color: arr[i].color
+            }
+          }
+
+          brackets = result;
+          console.log('brackets:');
+          console.log(brackets);
+        });
+
+    this.allGames.forEach((e) => {
+      e.pool_name = pools[e.pool_id]?.name || null;
+      e.type = pools[e.pool_id]?.name ? 'Pool' : null;
+      e.bracket_name = pools[e.bracket_id]?.name || null;
+      e.type = pools[e.pool_id]?.name ? 'Bracket' : null;
+
+      e.label_name = pools[e.pool_id]?.name || pools[e.bracket_id]?.name || null;
+    })
+
+    // Get unique names of pools & brackets
+    let unique_names = [...new Set(this.allGames.map((e) => e.label_name))];
+    unique_names = unique_names.filter(e => e !== null);
+    unique_names = ['All Teams'].concat(unique_names);
+
+    console.log('unique_names:');
+    console.log(unique_names);
+
+    // Create team mapping
+    let team_mapping = {};
+    for (let game of this.allGames) {
+      if (!(team_mapping.hasOwnProperty(game.curlingteam1_id))) {
+        team_mapping[game.curlingteam1_id] = game.team_name1
+      }
+      if (!(team_mapping.hasOwnProperty(game.curlingteam2_id))) {
+        team_mapping[game.curlingteam2_id] = game.team_name2
+      }
+    }
+
+    console.log(team_mapping);
+
+    let A = this.allGames.filter((e) => e.label_name === unique_names[1]);
+
+    // this.poolBracketList = [
+    //   [
+    //     'pool 1', [
+    //       {
+    //         name: team_name1,
+    //         wins: 0,
+    //         losses: 0,
+    //         ties: 0
+    //       },
+    //       {
+    //         name: team_name2,
+    //         wins: 0,
+    //         losses: 0,
+    //         ties: 0
+    //       }
+    //     ]
+    //   ],
+    //   ['pool 2'],
+    //   ['pool 3'],
+    //   ['bracket 1'],
+    //   ['bracket 2']
+    // ]
+
+    for (let event_type_name of unique_names) {
+
+      // console.log(event_type_name);
+
+      // Get all games in a given pool or bracket
+      let games = this.allGames.filter((e) => e.label_name === event_type_name);
+
+      // Get all unique teams in a given pool or bracket
+      let teams = [...new Set(games.map((e) => e.curlingteam1_id).concat(games.map((e) => e.curlingteam2_id)))];
+
+      console.log(teams);
+
+      // Add a counter object for each team
+      let to_add = [
+        event_type_name,  // Cattle-A
+        []                // List of team standings
+      ];
+
+      for (let team_id of teams) {
+        to_add[1].push({
+          team_name: team_mapping[team_id],
+          team_id: team_id,
+          wins: 0,
+          losses: 0,
+          ties: 0
+        })
+      }
+
+      // Tally up the scores
+      for (let game of games) {
+
+        // Increment ties...
+        if (game.finished === true && game.winner === null) {
+          to_add[1].find(e => e.team_id === game.curlingteam1_id).ties++;
+          to_add[1].find(e => e.team_id === game.curlingteam2_id).ties++;
+        }
+
+        // Increment win...
+        else if (game.finished === true && game.winner === game.curlingteam1_id) {
+          to_add[1].find(e => e.team_id === game.curlingteam1_id).wins++;
+          to_add[1].find(e => e.team_id === game.curlingteam2_id).losses++;
+        }
+
+        // Increment losses...
+        else if (game.finished === true && game.winner === game.curlingteam2_id) {
+          to_add[1].find(e => e.team_id === game.curlingteam2_id).wins++;
+          to_add[1].find(e => e.team_id === game.curlingteam1_id).losses++;
+        }
+
+      }
+
+      this.poolBracketList.push(to_add);
+    }
+
+    // Combine all lists for All Teams list
+
+
+    console.log(this.poolBracketList);
+
+    let totals = {};
+    for (let arr of this.poolBracketList.slice(1)) {
+      for (let game of arr[1]) {
+        if (!(totals.hasOwnProperty(game.team_id))) {
+          // console.log('creating');
+          // console.log(game);
+
+          totals[game.team_id] = game
+        } else {
+          // console.log('adding');
+          // console.log(totals[game.team_id]);
+          // console.log(game);
+
+          // console.log('before');
+          // console.log(totals[game.team_id].wins);
+
+
+          totals[game.team_id].wins += game.wins;
+          totals[game.team_id].losses += game.losses;
+          totals[game.team_id].ties += game.ties;
+
+          // console.log('after');
+          // console.log(totals[game.team_id].wins);
+
+
+        }
+      }
+    }
+
+    console.log(totals);
+
+    this.selectedPoolBracket = this.poolBracketList[0][0];
+    this.poolBracketList[0][1] = Object.values(totals);
+    this.dataSourceStandings = Object.values(totals);
+    this.dataSourceStandings.sort((a, b) => a.wins - b.wins).reverse();
+
+    console.log('this.dataSourceStandings');
+
+    console.log('poolBracketList:');
+    console.log(this.poolBracketList);
+
+    this.spinnerService.off();
   }
 
   openYoutubeDialog() {
@@ -490,6 +417,7 @@ export class DesktopViewComponent implements OnInit {
     console.log(event.value);
 
     this.dataSourceStandings = this.poolBracketList.find((e) => e[0] === event.value)[1];
+    this.dataSourceStandings.sort((a, b) => a.wins - b.wins).reverse();
   }
 }
 
--- src/app/modules/visitor/components/endscores/endscores.component.ts
+++ src/app/modules/visitor/components/endscores/endscores.component.ts
@@ -1,24 +1,6 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
-
-// export interface PeriodicElement {
-//   name: string;
-//   position: number;
-//   weight: number;
-//   symbol: string;
-// }
-
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-//   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-//   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-//   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-//   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-//   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-//   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-//   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-//   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-//   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-// ];
+import { TeamDialogOverviewComponent } from '@app/modules/visitor/components/team-dialog-overview/team-dialog-overview.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-endscores',
@@ -26,28 +8,31 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
   styleUrls: ['./endscores.component.scss']
 })
 export class EndscoresComponent implements OnInit, OnChanges {
-  // @Input() displayedColumns: string[];
-  // columnsToDisplay: string[] = [];
-  // data: PeriodicElement[] = ELEMENT_DATA;
-
   @Input() displayedColumns: string[];
   columnsToDisplay: string[] = [];
   @Input() data;
 
-  ngOnInit() {
-    // this.columnsToDisplay = this.displayedColumns.slice();
+  constructor(public dialog: MatDialog) { }
 
+  ngOnInit() { }
+
+  ngOnChanges() {
+    this.columnsToDisplay = this.displayedColumns.slice();
+
+    // console.log('ON CHANGES!');
     // console.log('displayedColumns', this.displayedColumns);
     // console.log('columnsToDisplay', this.columnsToDisplay);
     // console.log('data', this.data);
   }
 
-  ngOnChanges() {
-    this.columnsToDisplay = this.displayedColumns.slice();
 
-    console.log('ON CHANGES!');
-    console.log('displayedColumns', this.displayedColumns);
-    console.log('columnsToDisplay', this.columnsToDisplay);
-    console.log('data', this.data);
+  openDialog(): void {
+    const dialogRef = this.dialog.open(TeamDialogOverviewComponent, {
+      width: 'auto',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      // console.log('The dialog was closed');
+    });
   }
 }
