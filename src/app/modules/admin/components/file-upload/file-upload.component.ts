6acfc4b97cdf01952114082280d4bdb10f5b1383
David Kim <kim.david.j@gmail.com>
2020-07-31 14:38:18 -0700

    Added error handler to file-upload

--- src/app/modules/admin/components/file-upload/file-upload.component.ts
+++ src/app/modules/admin/components/file-upload/file-upload.component.ts
@@ -34,29 +34,36 @@ export class FileUploadComponent implements OnInit {
   }
 
   submitUpload() {
-    this.fileUploadService.uploadFile(this.form.value.fileData).subscribe(
-      (event: HttpEvent<any>) => {
-        switch (event.type) {
-          case HttpEventType.Sent:
-            console.log('Request has been made!');
-            this.notificationService.showInfo('Request has been made!', '');
-            break;
-          case HttpEventType.ResponseHeader:
-            console.log('Response header has been received!');
-            // this.notificationService.showInfo('Response header has been received!', '');
-            break;
-          case HttpEventType.UploadProgress:
-            this.progress = Math.round(event.loaded / event.total * 100);
-            console.log(`Uploaded! ${this.progress}%`);
-            // this.notificationService.showInfo(`${this.progress}%`, 'Progress');
-            break;
-          case HttpEventType.Response:
-            console.log('User successfully created!', event.body);
-            this.notificationService.showSuccess('File successfully uploaded!', '');
-            setTimeout(() => {
-              this.progress = 0;
-            }, 1500);
-        }
-      })
+    console.log('submitUpload() executed!');
+
+    this.fileUploadService.uploadFile(this.form.value.fileData)
+      .subscribe(
+        (event: HttpEvent<any>) => {
+          switch (event.type) {
+            case HttpEventType.Sent:
+              console.log('Request has been made!');
+              this.notificationService.showInfo('Request has been made!', '');
+              break;
+            case HttpEventType.ResponseHeader:
+              console.log('Response header has been received!');
+              // this.notificationService.showInfo('Response header has been received!', '');
+              break;
+            case HttpEventType.UploadProgress:
+              this.progress = Math.round(event.loaded / event.total * 100);
+              console.log(`Uploaded! ${this.progress}%`);
+              // this.notificationService.showInfo(`${this.progress}%`, 'Progress');
+              break;
+            case HttpEventType.Response:
+              console.log('User successfully created!', event.body);
+              this.notificationService.showSuccess('File successfully uploaded!', '');
+              setTimeout(() => {
+                this.progress = 0;
+              }, 1500);
+          }
+        },
+        (err) => {
+          console.log(err.message);
+          this.notificationService.showError(err.message, 'Something went wrong');
+        })
   }
 }
