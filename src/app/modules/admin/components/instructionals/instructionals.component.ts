b13a3100dfc2a3ff13c929a185d66cdcc67cffb6
David Kim <kim.david.j@gmail.com>
2020-07-31 15:19:28 -0700

    Added groundRules doc to instructionals component

--- src/app/modules/admin/components/instructionals/instructionals.component.ts
+++ src/app/modules/admin/components/instructionals/instructionals.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-instructionals',
-  templateUrl: './instructionals.component.html',
+  // templateUrl: './instructionals.component.html',
+  templateUrl: './groundRules.html',
   styleUrls: ['./instructionals.component.scss']
 })
 export class InstructionalsComponent implements OnInit {
