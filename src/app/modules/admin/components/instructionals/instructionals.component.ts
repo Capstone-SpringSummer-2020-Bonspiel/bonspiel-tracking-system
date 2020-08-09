7b8c8411002a576f89eb3be5e072ee2cf6f8bfa0
Frank Sun <franksun036@gmail.com>
2020-08-09 16:35:37 -0700

    pdf problem solved
    finally solved pdf file problem
    add pdf folder
    pdf files update

--- src/app/modules/admin/components/instructionals/instructionals.component.ts
+++ src/app/modules/admin/components/instructionals/instructionals.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./instructionals.component.scss']
 })
 export class InstructionalsComponent implements OnInit {
-  src = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf"
+  src = "https://nbviewer.jupyter.org/github/Capstone-SpringSummer-2020-Bonspiel/bonspiel-tracking-system/blob/dev/src/assets/pdf/Batch%20Upload%20Rule.pdf"
   // src = "assets\img\curler-silhouette.jpg"
 
   constructor() { }
