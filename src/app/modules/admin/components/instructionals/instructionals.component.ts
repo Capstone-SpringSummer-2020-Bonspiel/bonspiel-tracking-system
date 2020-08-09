46df73dcfb84ed6a3cfbfd10ffbd1da0cf7be374
Frank Sun <franksun036@gmail.com>
2020-08-09 16:47:46 -0700

    another slightly change

--- src/app/modules/admin/components/instructionals/instructionals.component.ts
+++ src/app/modules/admin/components/instructionals/instructionals.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./instructionals.component.scss']
 })
 export class InstructionalsComponent implements OnInit {
-  src = "https://nbviewer.jupyter.org/github/Capstone-SpringSummer-2020-Bonspiel/bonspiel-tracking-system/blob/dev/src/assets/pdf/BatchUploadRule.pdf"
+  src = "https://nbviewer.jupyter.org/github/Capstone-SpringSummer-2020-Bonspiel/bonspiel-tracking-system/blob/dev/src/assets/pdf/BUR.pdf"
   // src = "assets\img\curler-silhouette.jpg"
 
   constructor() { }
